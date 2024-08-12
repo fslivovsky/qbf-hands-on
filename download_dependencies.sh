f57ccb40951897915d1504b43263ca6b3aa7933e
Friedrich Slivovsky <fslivovsky@gmail.com>
2024-08-12 22:53:52 +0530

    download script for Nenofex and PicoSAT

---
+++ download_dependencies.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd depqbf
+
+if [ ! -d picosat ]
+then
+  echo "Downloading picosat version 960"
+  wget http://fmv.jku.at/picosat/picosat-960.tar.gz
+  tar -xvzf picosat-960.tar.gz
+  mv picosat-960 picosat
+else
+  echo "Found directory 'picosat'"
+fi
+
+if [ ! -d nenofex ]
+then
+    echo "Downloading nenofex version 1.1"
+    wget https://github.com/lonsing/nenofex/archive/version-1.1.tar.gz
+    tar -xvzf version-1.1.tar.gz
+    mv nenofex-version-1.1 nenofex
+else
+    echo "Found directory 'nenofex'"
+fi
